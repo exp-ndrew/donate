857a96d7d7812c6a8553b818cca0085ce24c565e
Matthew Williams & Andrew M Westling <student@epicodus.com>
2014-09-22 14:32:46 -0700

    add before_create method

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,11 @@ class Donation < ActiveRecord::Base
   belongs_to :nonprofit
   validates :amount, :presence => true
   validates :amount, :numericality => true
+  before_create :define_name
+
+private
+  def define_name
+    self.from = "Anonymous" unless self.from?
+  end
+
 end
